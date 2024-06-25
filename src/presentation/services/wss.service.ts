693d5a23c98299f524e078efa721401507d15c03
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2024-06-25 03:43:23 -0300

    online players movement

--- src/presentation/services/wss.service.ts
+++ src/presentation/services/wss.service.ts
@@ -29,12 +29,48 @@ export class WssService {
     WssService._instance = new WssService(options);
   }
 
+  players: any[] = [];
+  controlMap: any = {};
   public start() {
     this.wss.on("connection", (socket: Socket) => {
       console.log(`Client ${socket.id} connected`);
+
+      const player = {
+        x: 0,
+        y: 0,
+        id: socket.id,
+      };
+      this.players.push(player);
+
+      socket.on("controls", (controls) => {
+        this.controlMap[socket.id] = controls;
+      });
+
       socket.on("disconnect", () => {
+        this.players = this.players.filter((p) => p.id !== socket.id);
         console.log(`Client ${socket.id} disconnected`);
+        this.wss.emit("playerDisconnected", socket.id);
       });
     });
+
+    const tick = (delta: number) => {
+      for (const player of this.players) {
+        const playerControls = this.controlMap[player.id] ?? {};
+
+        if (playerControls["left"]) player.x -= 2;
+        if (playerControls["right"]) player.x += 2;
+        if (playerControls["up"]) player.y -= 2;
+        if (playerControls["down"]) player.y += 2;
+      }
+
+      this.wss.emit("players", this.players);
+    };
+
+    let lastUpdate = Date.now();
+    setInterval(() => {
+      const now = Date.now();
+      tick(now - lastUpdate);
+      lastUpdate = now;
+    }, 1000 / 30);
   }
 }
